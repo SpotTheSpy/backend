059b6aaf1d755ff3965822a66c7f457bc8f0affc
Plummy <plummybeatsoff@gmail.com>
2025-09-23 12:38:53 +0200

    Add users router

---
+++ app/api/v1/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from app.api.v1.users.router import users_router
+
+v1_router = APIRouter(prefix="v1")
+
+v1_router.include_router(users_router)
--- app/asgi.py
+++ app/asgi.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+from redis.asyncio import Redis
+
+from app.api.router import api_router
+from app.database.database import Database
 from config import Config
+
+
+@asynccontextmanager
+async def lifespan(fastapi_app: FastAPI):
+    yield
+    await fastapi_app.state.redis.close()
+
 
 app = FastAPI()
 
 config = Config(_env_file=".env")
+database = Database.from_dsn(config.database_dsn.get_secret_value())
+redis = Redis.from_url(config.redis_dsn.get_secret_value())
+
+app.state.config = config
+app.state.database = database
+app.state.redis = redis
+
+app.include_router(api_router)
