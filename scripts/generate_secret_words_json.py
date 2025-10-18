cb8e1b6c71c579e278eaabfec62ae529d68e40d9
Plummy <plummybeatsoff@gmail.com>
2025-10-18 03:33:23 +0200

    Update generate script

--- scripts/generate_secret_words_json.py
+++ scripts/generate_secret_words_json.py
@@ -1,26 +1,45 @@
 import logging
 from json import dump
-from typing import List, Dict
+from typing import List, Dict, Set
 
-__FILE_PATH: str = "app/assets/data/{file_name}"
+from app.assets.enums.category import Category
+
+
+def _get_all_secret_words() -> List[str]:
+    """
+    Retrieve all secret words from every category, without duplicated and in alphabetical order.
+
+    :return: List of all secret words.
+    """
+
+    secret_words: Set[str] = set()
+    categories: Set[Category] = {Category(category) for category in Category}
+
+    for category in categories:
+        filename: str = f"{category.lower()}.txt"
+        with open(f"app/assets/data/secret_words/{filename}", "r", encoding="utf-8") as file:
+            entries: List[str] = file.read().splitlines()
+        secret_words.update(entries)
+
+        logging.info(f"Retrieved {len(entries)} secret words from {category} category")
+
+    return list(sorted(secret_words))
 
 
 def main() -> None:
-    file_name: str = input("Enter file name: ")
-    if not file_name:
-        file_name = "secret_words"
-    file_name = file_name.removesuffix(".txt")
+    """
+    Retrieve all secret words from every category,
+    and create a JSON file with all keys set to secret word tags and values as formatted titled secret words.
+    """
 
-    with open(__FILE_PATH.format(file_name=f"{file_name}.txt"), "r", encoding="utf-8") as file:
-        secret_words: List[str] = file.read().splitlines()
-    logging.info(f"Exported {len(secret_words)} secret words")
+    secret_words: List[str] = _get_all_secret_words()
 
     json: Dict[str, str] = {secret_word: secret_word.title().replace("_", " ") for secret_word in secret_words}
     logging.info("Converted secret words to JSON")
 
-    with open(__FILE_PATH.format(file_name=f"{file_name}.json"), "w", encoding="utf-8") as file:
+    with open("scripts/output/generated_secret_words.json", "w", encoding="utf-8") as file:
         dump(json, file)
-    logging.info(f"Dumped JSON to file at {__FILE_PATH.format(file_name=f"{file_name}.json")}")
+    logging.info(f"Successfully dumped JSON")
 
 
 if __name__ == '__main__':
